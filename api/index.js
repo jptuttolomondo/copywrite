9b3784d3a964e015de43af4e8ec35adbc82a882e
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-07-31 15:50:05 -0300

    4

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js');
 const PORT=process.env.PORT|| 3001
-  server.listen(PORT, () => {
+  server.listen(PORT, () => { 
     console.log('server listening at 3001');
   });
 
